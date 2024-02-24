3f4795446e0c8d7e1f9728c46142b73a577843b1
myschkin <kenan.henzelin@proton.me>
2024-02-24 18:39:45 +0100

    added nvim correctly

---
+++ .config/nvim/after/plugin/copilot.lua
@@ -0,0 +1,12 @@
+require('copilot').setup({
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    accept = false,
+  },
+  panel = {
+    enabled = true,
+    auto_refresh = true
+  },
+})
+-- require('copilot_cmp').setup()
