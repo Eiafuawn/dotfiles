d490fc137b158bb2e7ff71387e6400f60237dd58
myschkin <kenan.henzelin@proton.me>
2024-03-03 20:36:02 +0100

    fixes

--- conf-install.sh
+++ conf-install.sh
@@ -5,14 +5,16 @@ conf=$base/.config
 home=$HOME
 
 # Clone suckless forks
-git clone https://github.com/Eiafuawn/dwm $conf/dwm
-git clone https://github.com/Eiafuawn/dmenu $conf/dmenu
-git clone https://github.com/Eiafuawn/dwmblocks $conf/dwmblocks
+git clone git@github.com:Eiafuawn/dwm.git $conf/dwm
+git clone git@github.com:Eiafuawn/dmenu.git $conf/dmenu
+git clone git@github.com:Eiafuawn/dwmblocks.git $conf/dwmblocks
 
 # Make install for each
 sudo make clean install -C $conf/dwm
 sudo make clean install -C $conf/dmenu
 sudo make clean install -C $conf/dwmblocks
+
+git clone https://github.com/tmux-plugins/tpm $base/tmux/plugins/tpm
 
 stow . -t $home
 
